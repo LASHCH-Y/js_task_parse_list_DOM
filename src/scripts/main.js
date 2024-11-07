7570632725a6d24db1378d409293ea258abacf85
unknown <yuli4ka.lashch98@gmail.com>
2024-11-07 19:03:11 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelector('ul');
+
+function sortList(listContainer) {
+  const listElements = Array.from(listContainer.children);
+
+  listElements.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  listElements.forEach((item) => listContainer.appendChild(item));
+}
+
+function parseSalary(salaryStr) {
+  return Number(salaryStr.replace(/[^0-9.-]+/g, ''));
+}
+
+sortList(employeeList);
